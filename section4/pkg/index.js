1126fe4bed280998719265d8da6e5930b57fe62c
pjnw1236 <pjnw1236@gmail.com>
2023-06-23 19:04:37 +0900

    [Section4]

---
+++ section4/pkg/index.js
@@ -0,0 +1,9 @@
+const randomColor = require("randomcolor");
+
+let color1 = randomColor();
+let color2 = randomColor();
+let color3 = randomColor();
+
+console.log(color1);
+console.log(color2);
+console.log(color3);
---
+++ section4/test/index.js
@@ -0,0 +1,4 @@
+const calc = require("./calc");
+
+console.log(calc.add(1, 2));
+console.log(calc.sub(10, 2));
