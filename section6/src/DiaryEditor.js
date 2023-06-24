663d3790516acc3411ee5ee42165a9fd3d0b218f
pjnw1236 <pjnw1236@gmail.com>
2023-06-24 17:25:54 +0900

    [Section6] 감정 선택 및 저장 구현

--- section6/src/DiaryEditor.js
+++ section6/src/DiaryEditor.js
@@ -4,6 +4,7 @@ const DiaryEditor = () => {
   const [state, setState] = useState({
     author: "",
     content: "",
+    emotion: 1,
   });
 
   const handleChangeState = (e) => {
@@ -14,6 +15,11 @@ const DiaryEditor = () => {
       ...state,
       [e.target.name]: e.target.value,
     });
+  };
+
+  const handleSubmit = (e) => {
+    console.log(state);
+    alert("저장 성공");
   };
 
   return (
@@ -32,6 +38,22 @@ const DiaryEditor = () => {
           value={state.content}
           onChange={handleChangeState}
         />
+      </div>
+      <div>
+        <select
+          name="emotion"
+          value={state.emotion}
+          onChange={handleChangeState}
+        >
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+        </select>
+      </div>
+      <div>
+        <button onClick={handleSubmit}>일기 저장하기</button>
       </div>
     </div>
   );
