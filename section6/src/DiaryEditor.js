87c7bdf85b2370df101a928fb6f8bb0f6d35fa82
pjnw1236 <pjnw1236@gmail.com>
2023-06-24 17:11:45 +0900

    [Section6] useState 묶어서 리팩토링

--- section6/src/DiaryEditor.js
+++ section6/src/DiaryEditor.js
@@ -1,27 +1,37 @@
 import { useState } from "react";
 
 const DiaryEditor = () => {
-  const [author, setAuthor] = useState("");
-  const [content, setContent] = useState("");
+  const [state, setState] = useState({
+    author: "",
+    content: "",
+  });
 
   return (
     <div className="DiaryEditor">
       <h2>오늘의 일기</h2>
       <div>
         <input
-          value={author}
+          value={state.author}
           onChange={(e) => {
             // console.log(e.target.name);
             // console.log(e.target.value);
-            setAuthor(e.target.value);
+            setState({
+              ...state,
+              author: e.target.value,
+              // content: state.content,
+            });
           }}
         />
       </div>
       <div>
         <textarea
-          value={content}
+          value={state.content}
           onChange={(e) => {
-            setContent(e.target.value);
+            setState({
+              ...state,
+              content: e.target.value,
+              // author: state.author,
+            });
           }}
         />
       </div>
