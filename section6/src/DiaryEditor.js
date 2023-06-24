e84b06406e22ecaeed5617340203ec51704de510
pjnw1236 <pjnw1236@gmail.com>
2023-06-24 17:15:23 +0900

    [Section6] handleChangeState 구현

--- section6/src/DiaryEditor.js
+++ section6/src/DiaryEditor.js
@@ -6,33 +6,31 @@ const DiaryEditor = () => {
     content: "",
   });
 
+  const handleChangeState = (e) => {
+    console.log(e.target.name);
+    console.log(e.target.value);
+
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <div className="DiaryEditor">
       <h2>오늘의 일기</h2>
       <div>
         <input
+          name="author"
           value={state.author}
-          onChange={(e) => {
-            // console.log(e.target.name);
-            // console.log(e.target.value);
-            setState({
-              ...state,
-              author: e.target.value,
-              // content: state.content,
-            });
-          }}
+          onChange={handleChangeState}
         />
       </div>
       <div>
         <textarea
+          name="content"
           value={state.content}
-          onChange={(e) => {
-            setState({
-              ...state,
-              content: e.target.value,
-              // author: state.author,
-            });
-          }}
+          onChange={handleChangeState}
         />
       </div>
     </div>
